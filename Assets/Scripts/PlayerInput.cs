6036d7763ed4c6109478425bebcaea42eb5017db
Voidraizer <splutikoff@gmail.com>
2015-04-25 03:07:54 -0400

    First commit\n testing

---
+++ Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerInput : MonoBehaviour {
+
+    public int healthIntensity;
+    public int damageIntensity;
+    public float jumpIntensity;
+    public float moveIntensity;
+
+    public GameObject playerCenter;
+    public GameObject GroundPoint;
+
+    public LayerMask Platforms;
+
+    private PlayerStats stats;
+
+    private Rigidbody2D rigidbod;
+
+    private bool ableToJump = true;
+
+    void Awake()
+    {
+        stats = GetComponent<PlayerStats>();
+        rigidbod = GetComponent<Rigidbody2D>();
+    }
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        float moveSpeed = stats.moveSpeed * moveIntensity;
+        float jumpSpeed = stats.jumpHeight * jumpIntensity;
+
+        // lock rotation
+        transform.localEulerAngles = new Vector3( transform.localEulerAngles.x, 0f, 0f );
+
+        // move left / right
+        if( Input.GetKey( KeyCode.A ) )
+        {
+            transform.position = new Vector3( transform.position.x - moveSpeed, transform.position.y, transform.position.z );
+        }
+        if( Input.GetKey( KeyCode.D) )
+        {
+            transform.position = new Vector3( transform.position.x + moveSpeed, transform.position.y, transform.position.z );
+        }
+
+        ableToJump = Physics2D.Linecast( playerCenter.transform.position, GroundPoint.transform.position, Platforms );
+        if( Input.GetKeyDown( KeyCode.W ) || Input.GetKeyDown( KeyCode.Space ) )
+        {
+            if( ableToJump )
+            {
+                rigidbod.AddForce( new Vector2( 0, jumpSpeed ), ForceMode2D.Impulse );
+            }
+        }
+	}
+
+
+}
---
+++ Assets/Scripts/PlayerStats.cs
@@ -0,0 +1,151 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PlayerStats : MonoBehaviour {
+
+    public Text HealthVal;
+    public Text DamageVal;
+    public Text MoveVal;
+    public Text JumpVal;
+    public Text ExtraPoints;
+
+    public int health;
+    public int damage;
+    public int moveSpeed;
+    public int jumpHeight;
+    public int totalPoints;
+    public int extraPoints;
+
+    
+
+    void Awake()
+    {
+
+    }
+
+	// Use this for initialization
+	void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        // change stats
+        // add
+        if( Input.GetKeyDown( KeyCode.U ) )
+        {
+            AddHealth();
+        }
+        else if( Input.GetKeyDown( KeyCode.I ) )
+        {
+            AddDamage();
+        }
+        else if( Input.GetKeyDown( KeyCode.O ) )
+        {
+            AddMovespeed();
+        }
+        else if( Input.GetKeyDown( KeyCode.P ) )
+        {
+            AddJumpHeight();
+        }
+
+        // subtract
+        else if( Input.GetKeyDown( KeyCode.J ) )
+        {
+            SubtractHealth();
+        }
+        else if( Input.GetKeyDown( KeyCode.K ) )
+        {
+            SubtractDamage();
+        }
+        else if( Input.GetKeyDown( KeyCode.L ) )
+        {
+            SubtractMovespeed();
+        }
+        else if( Input.GetKeyDown( KeyCode.Semicolon ) )
+        {
+            SubtractJumpHeight();
+        }
+
+        extraPoints = totalPoints - ( health + damage + moveSpeed + jumpHeight );
+
+        HealthVal.text   = health.ToString();
+        DamageVal.text   = damage.ToString();
+        MoveVal.text     = moveSpeed.ToString();
+        JumpVal.text     = jumpHeight.ToString();
+        ExtraPoints.text = extraPoints.ToString();
+	}
+
+    public void AddHealth()
+    {
+        if( extraPoints > 0 )
+        {
+            health++;
+            extraPoints--;
+        }
+    }
+
+    public void AddDamage()
+    {
+        if( extraPoints > 0 )
+        {
+            damage++;
+            extraPoints--;
+        }
+    }
+
+    public void AddMovespeed()
+    {
+        if( extraPoints > 0 )
+        {
+            moveSpeed++;
+            extraPoints--;
+        }
+    }
+
+    public void AddJumpHeight()
+    {
+        if( extraPoints > 0 )
+        {
+            jumpHeight++;
+            extraPoints--;
+        }
+    }
+
+    public void SubtractHealth()
+    {
+        if( health > 0 )
+        {
+            health--;
+            extraPoints++;
+        }
+    }
+
+    public void SubtractDamage()
+    {
+        if( damage > 0 )
+        {
+            damage--;
+            extraPoints++;
+        }
+    }
+
+    public void SubtractMovespeed()
+    {
+        if( moveSpeed > 0 )
+        {
+            moveSpeed--;
+            extraPoints++;
+        }
+    }
+
+    public void SubtractJumpHeight()
+    {
+        if( jumpHeight > 0 )
+        {
+            jumpHeight--;
+            extraPoints++;
+        }
+    }
+}
